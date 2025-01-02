8d511058883737f3fe1f85f39ea1c4f7787afa1a
Reeze <azfaalharits25@gmail.com>
2025-01-03 01:48:55 +0700

    chore: add .env

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import express, { Application } from "express";
 import cors from "cors";
 import router from "./routes/api";
 import validateMethodType from "./utils/helpers/method";
+import dotenv from "dotenv";
 
 let app: Application = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(validateMethodType);
 app.use("/api", router);
 
-const PORT: number = 8000;
+const PORT: number | string = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
