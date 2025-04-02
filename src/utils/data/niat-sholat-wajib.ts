556b13303695ee5d3f8268bdaa7598ee91d28cde
Reeze <azfaalharits25@gmail.com>
2025-04-03 01:30:14 +0700

    refactor: property name in niat sholat wajib

--- src/utils/data/niat-sholat-wajib.ts
+++ src/utils/data/niat-sholat-wajib.ts
@@ -1,79 +1,79 @@
 export const niatSholatWajib: Array<{
-    jenisSholat: string;
+    namaSholat: string;
     niatSholat: string;
-    arab: string;
-    latin: string;
-    arti: string;
+    teksArab: string;
+    teksLatin: string;
+    artinya: string;
   }> = [
     {
-        jenisSholat: "Subuh",
+        namaSholat: "Subuh",
         niatSholat: "Niat sholat Subuh sendiri",
-        arab: "أُصَلِّى فَرْضَ الصُّبْح رَكَعتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً لله تَعَالَى",
-        latin: "Arab-latin: Usholli fardlon shubhi rok'ataini mustaqbilal qiblati adaa-an lillahi Ta'aala.",
-        arti: "Aku niat melakukan sholat fardhu subuh dua rakaat, sambil menghadap kiblat, karena Allah Ta'ala."
+        teksArab: "أُصَلِّى فَرْضَ الصُّبْح رَكَعتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً لله تَعَالَى",
+        teksLatin: "Usholli fardlon shubhi rok'ataini mustaqbilal qiblati adaa-an lillahi Ta'aala.",
+        artinya: "Aku niat melakukan sholat fardhu subuh dua rakaat, sambil menghadap kiblat, karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Subuh",
+        namaSholat: "Subuh",
         niatSholat: "Niat sholat Subuh berjamaah",
-        arab: "اُصَلّى فَرْضَ الصُّبْحِ رَكْعَتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushollii fardhol shubhi rok'ataini mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi Ta'aala.",
-        arti: "Aku niat sholat fardhu subuh dua rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
+        teksArab: "اُصَلّى فَرْضَ الصُّبْحِ رَكْعَتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَى",
+        teksLatin: "Ushollii fardhol shubhi rok'ataini mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi Ta'aala.",
+        artinya: "Aku niat sholat fardhu subuh dua rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Dzuhur",
+        namaSholat: "Dzuhur",
         niatSholat: "Niat sholat Dzuhur sendiri",
-        arab: "أصَلِّي فَرْضَ الظَّهْرِ أَرْبَعَ رَكَعَاتٍ مُستَقبِلَ الْقِبْلَةِ آداء لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushalli fardhadz dzuhri arba'a raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'aala.",
-        arti: "Saya niat sholat fardhu Dzuhur empat rakaat menghadap kiblat karena Allah Ta'ala."
+        teksArab: "أصَلِّي فَرْضَ الظَّهْرِ أَرْبَعَ رَكَعَاتٍ مُستَقبِلَ الْقِبْلَةِ آداء لِلَّهِ تَعَالَى",
+        teksLatin: "Ushalli fardhadz dzuhri arba'a raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'aala.",
+        artinya: "Saya niat sholat fardhu Dzuhur empat rakaat menghadap kiblat karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Dzuhur",
+        namaSholat: "Dzuhur",
         niatSholat: "Niat sholat Dzuhur berjamaah",
-        arab: "ُصَلِّي فَرْضَ الظُّهْرِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا لِإِمَامًا لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushollii fardhozh zhuhri arba'a roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi ta'aala.",
-        arti: "Aku niat sholat fardhu Dzuhur empat rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
+        teksArab: "ُصَلِّي فَرْضَ الظُّهْرِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا لِإِمَامًا لِلَّهِ تَعَالَى",
+        teksLatin: "Ushollii fardhozh zhuhri arba'a roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi ta'aala.",
+        artinya: "Aku niat sholat fardhu Dzuhur empat rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Ashar",
+        namaSholat: "Ashar",
         niatSholat: "Niat sholat Ashar sendiri",
-        arab: "أصَلِّي فَرْضَ الْعَصْرِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushalli fardhal 'ashri arba'a raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'ala.",
-        arti: "Saya niat sholat fardhu Ashar empat rakaat menghadap kiblat karena Allah Ta'ala."
+        teksArab: "أصَلِّي فَرْضَ الْعَصْرِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً لِلَّهِ تَعَالَى",
+        teksLatin: "Ushalli fardhal 'ashri arba'a raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'ala.",
+        artinya: "Saya niat sholat fardhu Ashar empat rakaat menghadap kiblat karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Ashar",
+        namaSholat: "Ashar",
         niatSholat: "Niat sholat Ashar berjamaah",
-        arab: "ُصَلِّي فَرْضَ الْعَصْرِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushollii fardhol 'ashri arba'a roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi Ta'aala.",
-        arti: "Aku niat sholat fardhu Ashar empat rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
+        teksArab: "ُصَلِّي فَرْضَ الْعَصْرِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَى",
+        teksLatin: "Ushollii fardhol 'ashri arba'a roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi Ta'aala.",
+        artinya: "Aku niat sholat fardhu Ashar empat rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Maghrib",
+        namaSholat: "Maghrib",
         niatSholat: "Niat sholat Maghrib sendiri",
-        arab: "َصَلِّي فَرْضَ الْمَغْرِبِ ثَلَاثَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushalli fardhal maghribi tsalaatsa raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'aala.",
-        arti: "Saya niat sholat fardhu Maghrib tiga rakaat menghadap kiblat karena Allah Ta'ala."
+        teksArab: "َصَلِّي فَرْضَ الْمَغْرِبِ ثَلَاثَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً لِلَّهِ تَعَالَى",
+        teksLatin: "Ushalli fardhal maghribi tsalaatsa raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'aala.",
+        artinya: "Saya niat sholat fardhu Maghrib tiga rakaat menghadap kiblat karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Maghrib",
+        namaSholat: "Maghrib",
         niatSholat: "Niat sholat Maghrib berjamaah",
-        arab: "صَلِّ فَرْضَ الْمَغْرِبِ ثَلَاثَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushollii fardhol maghribi tsalaatsa roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi ta'aalaa.",
-        arti: "Saya niat sholat fardhu Maghrib tiga rakaat menghadap kiblat menjadi makmum/imam karena Allah Ta'ala."
+        teksArab: "صَلِّ فَرْضَ الْمَغْرِبِ ثَلَاثَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَى",
+        teksLatin: "Ushollii fardhol maghribi tsalaatsa roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi ta'aalaa.",
+        artinya: "Saya niat sholat fardhu Maghrib tiga rakaat menghadap kiblat menjadi makmum/imam karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Isya",
+        namaSholat: "Isya",
         niatSholat: "Niat sholat Isya sendiri",
-        arab: "َصَلِّي فَرْضَ الْعِشَاءِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً لِلَّهِ تَعَالَى",
-        latin: "Arab-latin: Ushalli fardhal Isyaa'i arba'a raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'aala.",
-        arti: "Saya niat sholat fardhu Isya empat rakaat menghadap kiblat karena Allah Ta'ala."
+        teksArab: "َصَلِّي فَرْضَ الْعِشَاءِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً لِلَّهِ تَعَالَى",
+        teksLatin: "Ushalli fardhal Isyaa'i arba'a raka'aatin mustaqbilal qiblati adaa'an lillaahi Ta'aala.",
+        artinya: "Saya niat sholat fardhu Isya empat rakaat menghadap kiblat karena Allah Ta'ala."
     },
     {
-        jenisSholat: "Isya",
+        namaSholat: "Isya",
         niatSholat: "Niat sholat Isya berjamaah",
-        arab: "ُصَلِّي فَرْضَ الْعِشَاءِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَ",
-        latin: "Arab-latin: Ushollii fardhol isyaa'i arba'a roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi ta'aala.",
-        arti: "Saya niat sholat fardhu Isya empat rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
+        teksArab: "ُصَلِّي فَرْضَ الْعِشَاءِ أَرْبَعَ رَكَعَاتٍ مُسْتَقْبِلَ الْقِبْلَةِ آدَاءً مَأْمُوْمًا إِمَامًا لِلَّهِ تَعَالَ",
+        teksLatin: "Ushollii fardhol isyaa'i arba'a roka'aatin mustaqbilal qiblati adaa-an ma'muuman/imaaman lillaahi ta'aala.",
+        artinya: "Saya niat sholat fardhu Isya empat rakaat menghadap kiblat, menjadi makmum/imam karena Allah Ta'ala."
     },
 ]
 
